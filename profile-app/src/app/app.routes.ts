40f99dafcf12faa32337e6138e2bca71aaea37dd
codingerkitten <codingercat@gmail.com>
2024-12-07 01:50:56 +0800

    dashboard component, route update, app.c update

--- profile-app/src/app/app.routes.ts
+++ profile-app/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'logout', component: LogoutComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'profile', component: ProfileComponent}
+    {path: 'profile', component: ProfileComponent},
+    {path: 'dashboard', component: DashboardComponent}
 ];
